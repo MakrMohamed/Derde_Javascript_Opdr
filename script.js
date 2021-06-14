b652e29fd736ecb2e0e8e431ef6455d50edfbffc
MakrMohamed <84314376+MakrMohamed@users.noreply.github.com>
2021-06-14 11:00:05 +0200

    Add files via upload
    3e Javascript opdr

---
+++ script.js
@@ -0,0 +1,25 @@
+const age = 30;
+if (age >= 18) {
+    console.log("U mag naar binnen");
+
+} else if (age < 18) {
+    console.log("Helaas, u mag nog niet naar binnen");
+}
+
+
+const isFemale = false;
+if (isFemale) {
+    console.log("Welkom bij de ladiesnight");
+
+} else {
+    console.log("Helaas, dit feest is alleen voor vrouwen");
+}
+
+
+const driverStatus = 'bob';
+if (driverStatus == 'bob') {
+    console.log("U mag rijden, wel thuis!");
+
+} else {
+    console.log("Helaas, u mag niet rijden. We bestellen een taxi voor u");
+}
